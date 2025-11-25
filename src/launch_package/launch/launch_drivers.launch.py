fc3071b8f8b597a623adcef2c8004225bbe3ebf0
NielsGoBrrr <nielserikvandam@gmail.com>
2025-11-25 11:54:17 +0100

    First working version via DriverLaunch, applaunch not tested yet.
    Generally contains a large amount of file edits & similar. Please note:
    The rviz file is most definitely not correct, and the gripper is still
    fully absent.

--- src/launch_package/launch/launch_drivers.launch.py
+++ src/launch_package/launch/launch_drivers.launch.py
@@ -1,3 +1,4 @@
+from traitlets import default
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
@@ -19,7 +20,7 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             'robot_ip',
-            default_value='192.168.10.40', # <<! ABB - Change IP to match service port host address. >>
+            default_value='192.168.10.11', # <<! ABB - Change IP to match service port host address. >>
             description='Robot IP address'
         )
     )
@@ -30,24 +31,33 @@ def generate_launch_description():
             description='Controller type'
         )
     )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            'moveit_config_package',
+            default_value='abb_irb120_moveit_config',
+            description='MoveIt configuration package for the robot, e.g. abb_irb1200_5_90_moveit_config'
+        )
+    )
 
     # Initialize Arguments
     use_fake_hardware = LaunchConfiguration('use_fake_hardware')
     robot_ip = LaunchConfiguration('robot_ip')
     controller_type = LaunchConfiguration('controller_type')
+    moveit_config_package = LaunchConfiguration('moveit_config_package')
 
-    melfa_driver_launch = IncludeLaunchDescription(
+    abb_driver_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(get_package_share_directory('abb_bringup'), 'launch', 'abb_control.launch.py') # Was originally "melfa_bringup" and "rv5as_control_custom.launch.py"
         ),
         launch_arguments={
             'use_fake_hardware': use_fake_hardware,
             'controller_type': controller_type,
+            'moveit_config_package' : moveit_config_package,
             'robot_ip': robot_ip
         }.items()
     )
 
-    melfa_movit_launch = IncludeLaunchDescription(
+    abb_movit_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource( 
             os.path.join(get_package_share_directory('abb_bringup'), 'launch', 'abb_moveit.launch.py') # Was "melfa_rv5as_moveit_config" and "rv5as_moveit.launch.py"
         )
@@ -60,8 +70,8 @@ def generate_launch_description():
     )
 
     launches = [
-        melfa_driver_launch, # <<! Change name, looks better. >>
-        melfa_movit_launch, # <<! Change name, looks better. >>
+        abb_driver_launch, # <<! Change name, looks better. >>
+        abb_movit_launch, # <<! Change name, looks better. >>
         standalone_trajectory_executor_launch
     ] 
 
